3d79c60d781b10ffa3d3614f78491975e8da3939
amahmud <amahmud@fastenal.com>
2021-08-13 16:16:05 -0500

    fixed

--- src/main/java/org/springframework/samples/petclinic/model/BaseEntity.java
+++ src/main/java/org/springframework/samples/petclinic/model/BaseEntity.java
@@ -31,7 +31,6 @@ import javax.persistence.MappedSuperclass;
  */
 @MappedSuperclass
 public class BaseEntity implements Serializable {
-// comment
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
